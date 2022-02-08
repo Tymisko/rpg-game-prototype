a5ea8185a16f8ac0c4da0a04d474990c1d3f30ab
Jan Urbaś <janurbas39@gmail.com>
2022-02-08 09:17:56 +0100

    Add static class that stores tags

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tags
+{
+    public const string Ground = "Ground";
+    public const string Item = "Item";
+}
