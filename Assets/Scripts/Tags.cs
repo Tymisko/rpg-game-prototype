994ddbfab2d991cf8b4501479d5237b4b0925a64
Jan Urbaś <janurbas39@gmail.com>
2022-02-14 08:29:13 +0100

    Remove all files

--- Assets/Scripts/Tags.cs
+++
@@ -1,9 +0,0 @@
-namespace Assets.Scripts
-{
-    public static class Tags
-    {
-        public const string Ground = "Ground";
-        public const string Item = "Item";
-        public const string Player = "Player";
-    }
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# A RPG game prototype project
-
-[Design Doc](https://docs.google.com/document/d/16XN2AGRpR33z2dRUUNEGYTmxew5E5F8QFmfnYv8YMcM/edit)
